4724fd1d032788fb5f1b1371ceca4d300e35861f
Eran Matityahu <eran.m@variscite.com>
2017-07-12 17:30:21 +0300

    imx6ul-var-dart: jig scripts: bt_test: Remove the 3 times loop

--- scripts/jig_scripts/bt_test.sh
+++ scripts/jig_scripts/bt_test.sh
@@ -1,26 +1,27 @@
 #! /bin/sh
+
+FIRMWARE=/lib/firmware/bcm/bcm43430a1.hcd
+
 echo 132 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio132/direction
-for i in 1 2 3
-do
-#Three times load firmware check. If pass then UART soldered well
-        killall -q brcm_patchram_plus
-        echo 0 > /sys/class/gpio/gpio132/value
-        sleep 1
-        echo 1 > /sys/class/gpio/gpio132/value
 
-        brcm_patchram_plus --patchram /lib/firmware/bcm/bcm43430a1.hcd  --enable_hci --bd_addr 64:a3:cb:5b:69:f0 --no2bytes --tosleep 1000 /dev/ttymxc1 &
+killall -q brcm_patchram_plus
 
-        sleep 10
+echo 0 > /sys/class/gpio/gpio132/value
+sleep 1
+echo 1 > /sys/class/gpio/gpio132/value
 
-        hciconfig hci0 up
+brcm_patchram_plus --patchram $FIRMWARE --enable_hci --bd_addr 64:a3:cb:5b:69:f0 --no2bytes --tosleep 1000 /dev/ttymxc1 &
 
-        hcitool dev
-        RET=`hcitool dev | grep hci -c`
-        echo $RET
-        if [ $RET = "0" ]; then
-                echo FAIL Device is not up
-                exit
-        fi
-done
+sleep 10
+
+hciconfig hci0 up
+
+hcitool dev
+RET=`hcitool dev | grep hci -c`
+echo $RET
+if [ $RET = "0" ]; then
+	echo FAIL Device is not up
+	exit
+fi
 echo PASS
